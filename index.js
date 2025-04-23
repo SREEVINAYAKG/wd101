0033ea7003c46b82ef9fa4eb011fc29ffda7919f
G Sree Vinayak <146360497+SREEVINAYAKG@users.noreply.github.com>
2025-04-23 11:54:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,8 +47,6 @@ const saveUserForm=(event)=>{
     const dob=document.getElementById("dob").value;
     const terms=document.getElementById("terms").checked;
 
-    // const email = document.getElementById("email");
-
     email.addEventListener("input", function (event) {
     if (email.validity.typeMismatch) {
     email.setCustomValidity("This is not a valid email address!");
