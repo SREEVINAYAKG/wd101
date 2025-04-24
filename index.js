86bf5e1764f56137025dfd90c18159a3ca151a60
G Sree Vinayak <146360497+SREEVINAYAKG@users.noreply.github.com>
2025-04-24 20:19:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,28 +72,28 @@ displayEntries();
 
 window.onload = function() {
     var date = new Date();
-    var dd = date.getDate();
-    var mm = date.getMonth() + 1;
+    // var dd = date.getDate();
+    // var mm = date.getMonth() + 1;
     var yyyy = date.getFullYear();
 
     //Add a zero if one Digit (eg: 05,09)
-    if (dd < 10) {
-      dd = "0" + dd;
-    }
+    // if (dd < 10) {
+    //   dd = "0" + dd;
+    // }
 
-    //Add a zero if one Digit (eg: 05,09)
-    if (mm < 10) {
-      mm = "0" + mm;
-    }
+    // //Add a zero if one Digit (eg: 05,09)
+    // if (mm < 10) {
+    //   mm = "0" + mm;
+    // }
 
     minYear = yyyy - 55; //Calculate Minimun Age (<80)
     maxYear = yyyy - 18; //Calculate Maximum Age (>18)
 
-    var min = minYear + "-" + mm + "-" + dd;
-    var max = maxYear + "-" + mm + "-" + dd;
+    // var min = minYear + "-" + mm + "-" + dd;
+    // var max = maxYear + "-" + mm + "-" + dd;
 
-    document.getElementById("dob").setAttribute("min", min);
-    document.getElementById("dob").setAttribute("max", max);
+    document.getElementById("dob").setAttribute("minYear", minYear);
+    document.getElementById("dob").setAttribute("maxYear", maxYear);
   };
 
 
